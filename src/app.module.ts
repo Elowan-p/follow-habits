5ea3fedfc99b9e946479f800150f1df4da9d84d6
Elowan <elowanpaino@gmail.com>
2026-01-21 13:39:24 +0100

    class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './context/users/users.module';
+import { HabitsModule } from './context/habits/habits.module';
+import { TrackingModule } from './context/tracking/tracking.module';
+import { StatsModule } from './context/stats/stats.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, HabitsModule, TrackingModule, StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
